ef1a51f6b3306d5705a386b85d2cf8aa3541020e
Jan-Philipp Marks <jpm3492@gmail.com>
2018-07-24 20:52:04 +0200

    all dev parts out

--- router.js
+++ router.js
@@ -7,9 +7,6 @@ const requireAuth = passport.authenticate('jwt', { session: false })
 const requireSignin = passport.authenticate('local', { session: false })
 
 module.exports = function(app) {
-  app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-  });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
   app.get('/schedule', requireAuth, Bookings.schedule)
