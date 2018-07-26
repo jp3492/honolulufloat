4d412846afa11297fe37f94db2e93b174e5bfa32
Jan-Philipp Marks <jpm3492@gmail.com>
2018-07-26 22:33:58 +0200

    check if booking exists

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json({ type: '*/*' }))
 app.use((req, res, next) => {
+  console.log(req.body);
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
